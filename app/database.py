9309c6141e05045db527540677a3db29ca21fce1
Orangeliquid <hardym2020111@gmail.com>
2025-05-06 03:03:31 -0400

    Add enable_sqlite_foreign_keys event_listener to enable foreignkey constraints

--- app/database.py
+++ app/database.py
@@ -1,10 +1,20 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, declarative_base
+import sqlite3
 
 DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+# Enable ForeignKey constraints in SQLite
+@event.listens_for(engine, "connect")
+def enable_sqlite_foreign_keys(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
 
 
 def get_db():
